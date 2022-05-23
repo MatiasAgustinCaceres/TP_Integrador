8c5110a61fc185bc123fdbb4112a52195c1cc26e
Andy Molina <andy10134@hotmail.com>
2022-05-23 18:47:39 -0300

    atomize funcionando

--- golb/src/index.js
+++ golb/src/index.js
@@ -1,17 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { StyleReset } from "atomize";
+import { Provider as StyletronProvider, DebugEngine } from "styletron-react";
+import { Client as Styletron } from "styletron-engine-atomic";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const debug =
+  process.env.NODE_ENV === "production" ? void 0 : new DebugEngine();
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+// 1. Create a client engine instance
+const engine = new Styletron();
+
+// 2. Provide the engine to the app
+// debug engine needs inlined source maps
 root.render(
   <React.StrictMode>
-    <App />
+    <StyletronProvider value={engine} debug={debug} debugAfterHydration>
+      <StyleReset></StyleReset>
+      <App />
+    </StyletronProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
